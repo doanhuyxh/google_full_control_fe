358b7745237635dc187c67182b1114ce6a28078b
doanhuyxh <doanhuyxh@gmail.com>
2026-06-29 14:27:13 +0700

    refactor: convert AdminLayout to a client component and simplify its structure

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import { ReactNode } from "react";
 import ReduxProvider from "@/components/providers/ReduxProvider";
 import ProtectedLayout from "@/components/layout/Protected";
 
-export default async function AdminLayout({ children }: { children: ReactNode }) {
+export default function AdminLayout({ children }: { children: ReactNode }) {
     return (
         <ProtectedLayout>
             <ReduxProvider>
